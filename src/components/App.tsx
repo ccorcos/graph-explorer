dc3cf3fd9ad37d2f9cfa20b6fd6f6183fba114b4
Chet Corcos <ccorcos@gmail.com>
2017-10-25 20:51:37 -0700

    Basic mapping

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,34 @@
 import * as React from "react"
 import Component from "reactive-magic/component"
+import dependencies from "../dependencies"
+
+const childMap: { [key: string]: Set<string> } = {}
+const parentMap: { [key: string]: Set<string> } = {}
+
+dependencies.dependencies.forEach(parentDep => {
+	const parent = parentDep.source
+	if (!childMap[parent]) {
+		childMap[parent] = new Set()
+	}
+	parentDep.dependencies.forEach(childDep => {
+		const child = childDep.resolved
+		if (!parentMap[child]) {
+			parentMap[child] = new Set()
+		}
+		childMap[parent].add(child)
+		parentMap[child].add(parent)
+	})
+})
+
+const root = "src/client/main.ts"
 
 export default class App extends Component<{}> {
 	view() {
 		return (
 			<div>
-				<h1>Hello World</h1>
+				{Array.from(childMap[root]).map(source => {
+					return <div key={source}>{source}</div>
+				})}
 			</div>
 		)
 	}
