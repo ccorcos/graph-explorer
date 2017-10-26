8de0e426b98c60480a7eda9f5cab44b9919bb283
Chet Corcos <ccorcos@gmail.com>
2017-10-25 21:11:00 -0700

    First round of ideas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Component from "reactive-magic/component"
+import { Value } from "reactive-magic"
 import dependencies from "../dependencies"
 
 const childMap: { [key: string]: Set<string> } = {}
@@ -20,15 +21,50 @@ dependencies.dependencies.forEach(parentDep => {
 	})
 })
 
-const root = "src/client/main.ts"
+// A column is either a parent of X, child of X, or root of X.
+// The anchor determines which column splits parent and child.
+
+// we need a list of columns
+// the anchor is the column index
+// columns need to know their index in the global
+
+interface ColumnType {
+	type: "parent" | "child" | "root"
+	key: string
+}
 
 export default class App extends Component<{}> {
+	private columns: Value<Array<ColumnType>>
+	private anchor: Value<number>
+
+	constructor(props) {
+		super(props)
+		const root = "src/client/stores/RecordStore.ts"
+		const columns: Array<ColumnType> = [
+			{ type: "parent", key: root },
+			{ type: "root", key: root },
+			{ type: "child", key: root },
+		]
+		const anchor = 1
+		this.columns = new Value(columns)
+		this.anchor = new Value(anchor)
+	}
 	view() {
 		return (
-			<div>
-				{Array.from(childMap[root]).map(source => {
-					return <div key={source}>{source}</div>
-				})}
+			<div style={{ display: "flex" }}>
+				<div style={{ padding: 12 }}>
+					{Array.from(parentMap[this.root.get()]).map(source => {
+						return <div key={source}>{source}</div>
+					})}
+				</div>
+				<div style={{ padding: 12 }}>
+					<div>{this.root.get()}</div>
+				</div>
+				<div style={{ padding: 12 }}>
+					{Array.from(childMap[this.root.get()]).map(source => {
+						return <div key={source}>{source}</div>
+					})}
+				</div>
 			</div>
 		)
 	}
